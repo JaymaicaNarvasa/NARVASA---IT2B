a54eaa425cd308a7a77a1e55c538d7447177c890
JaymaicaNarvasa <maicamaica211@gmail.com>
2024-09-12 05:23:16 -0700

    Hotels.java

--- Narvasa/Narvasa/src/narvasa/Hotels.java
+++ Narvasa/Narvasa/src/narvasa/Hotels.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package narvasa;
 
-/**
- *
- * @author SCC-COLLEGE
- */
 public class Hotels {
+    String name, rtype, pstats, status;
+    int id, cout,cin;
     
+    public void getHotels(String Name , String Rtype, String Pstats, String Status, int Id, int Cout, int  Cin) {
+        this.name = Name;
+        this.rtype = Rtype;
+        this. pstats = Pstats;
+        this.status = Status;
+        this.id = Id;
+        this.cout = Cout;
+        this.cin = Cin;
+        
+    }
+    
+    public void viewHotel(){
+        System.out.printf("|  %-10d  |  %-10s  |  %-10s  |  %-10d  |  %-10d  |  %-10s  |  %-10s  |",  this.name, this.rtype,this. pstats, this.status, this.id,this.cout, this.cin );
+    }
 }
